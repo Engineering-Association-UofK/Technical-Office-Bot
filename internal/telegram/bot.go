ad5d49df23b9358d4a4a5f6dd2f210f9132d0cbb
Abdulrahim M. <unvacc80@gmail.com>
2026-02-08 03:43:23 +0200

    Implement basic Telegram bot service.

---
+++ internal/telegram/bot.go
@@ -0,0 +1,71 @@
+package telegram
+
+import (
+	"log"
+
+	"github.com/abdulrahim-m/Technical-Office-Bot/internal/local"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type TelegramBot struct {
+	bot     *tgbotapi.BotAPI
+	adminID int64
+	local   local.TelegramLocal
+}
+
+func TelegramInit(token string, adminID int64) *TelegramBot {
+	log.Println("Initializing Telegram bot...")
+
+	// Initialize the bot
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic("Error initializing telegram bot: ", err)
+	}
+
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	t := TelegramBot{
+		bot:     bot,
+		adminID: adminID,
+	}
+
+	// Load Default Local
+	t.local.Load()
+
+	// Begin listening to incoming messages
+	go t.Listen()
+
+	return &t
+}
+
+func (t *TelegramBot) Listen() {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := t.bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			switch update.Message.Text {
+			case "/start":
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, t.local.WelcomeMessage)
+				msg.ParseMode = "Markdown"
+				t.bot.Send(msg)
+
+			default:
+				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+				msg.ReplyToMessageID = update.Message.MessageID
+
+				t.bot.Send(msg)
+			}
+		}
+	}
+}
+
+func (t *TelegramBot) NotifyAdmin(message string) {
+	msg := tgbotapi.NewMessage(t.adminID, message)
+	t.bot.Send(msg)
+}
